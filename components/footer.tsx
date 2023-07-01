c90830224c79399fb42e1b977e471eacd52ce32d
Ryan Key <realryankey@gmail.com>
2023-07-01 15:27:28 -0500

    updates

--- components/footer.tsx
+++ components/footer.tsx
@@ -7,9 +7,55 @@ const Footer = () => {
       <Container>
         <div className="py-28 flex flex-col lg:flex-row items-center">
           <h3 className="text-4xl lg:text-[2.5rem] font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
-            Statically Generated with Next.js.
+            We only do search! 
           </h3>
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
+          <div className="container">
+            <div className="row">
+        <div className="col-md-4">
+          <span className="copyright">Copyright &copy; Reaction Agency 2023</span>
+        </div>
+        <div className="col-md-4">
+          <ul className="list-inline social-buttons">
+            <li className="list-inline-item">
+              <a href="#">
+                <i className="fab fa-twitter"></i>
+              </a>
+            </li>
+            <li className="list-inline-item">
+              <a href="#">
+                <i className="fab fa-facebook-f"></i>
+              </a>
+            </li>
+            <li className="list-inline-item">
+              <a href="#">
+                <i className="fab fa-instagram"></i>
+              </a>
+            </li>
+            <li className="list-inline-item">
+              <a href="#">
+                <i className="fab fa-linkedin-in"></i>
+              </a>
+            </li>
+            <li className="list-inline-item">
+              <a href="#">
+                <i className="fab fa-medium"></i>
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="col-md-4">
+          <ul className="list-inline quicklinks">
+            <li className="list-inline-item">
+              <a href="#">Privacy Policy</a>
+            </li>
+            <li className="list-inline-item">
+              <a href="#">Terms of Use</a>
+            </li>
+          </ul>
+        </div>
+            </div>
+            </div>
             <a
               href="https://nextjs.org/docs/basic-features/pages"
               className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
