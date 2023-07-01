da3b9786e70819f81d218233c9d515b0d92f534f
Ryan Key <realryankey@gmail.com>
2023-07-01 15:35:50 -0500

    update

--- components/intro.tsx
+++ components/intro.tsx
@@ -6,9 +6,10 @@ const Intro = () => {
       <h1 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
         Searchable
       </h1>
-      <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        An SEO Agency
-      </h4>
+      <a href="https://nextjs.org/docs/basic-features/pages"
+        className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0">
+            Get a Quote
+            </a>
     </section>
   )
 }
